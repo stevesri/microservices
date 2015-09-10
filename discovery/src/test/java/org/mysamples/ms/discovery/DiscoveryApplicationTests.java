d5e6f5576d74e3c98f24e5d05d421767dc8598ab
Siram Vasantha <svasantha@137IT1FL49446W7.dsnyad.nycnet>
2015-09-10 14:13:19 -0400

    :kg change

---
+++ discovery/src/test/java/org/mysamples/ms/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,17 @@
+package org.mysamples.ms.discovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mysamples.ms.discovery.DiscoveryApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DiscoveryApplication.class)
+public class DiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
